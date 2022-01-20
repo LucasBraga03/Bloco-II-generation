6ef06e9c3c367e0fdd21f79b842088287ea2c5db
Lucas Silva Braga <lucas.sbraga14@gmail.com>
2022-01-20 19:56:44 -0300

    Spring

---
+++ Spring/blogpessoal/blogpessoal/src/main/java/br/org/generation/blogpessoal/repository/TemaRepository.java
@@ -0,0 +1,17 @@
+package br.org.generation.blogpessoal.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.org.generation.blogpessoal.model.TemaModel;
+
+@Repository
+public interface TemaRepository extends JpaRepository <TemaModel, Long >{
+
+	
+	List<TemaModel> findAllByDescricaoContainingIgnoreCase(String descricao);
+ 
+	
+}
