13607affc600c4eca762d06549cb0f7568c707f3
Lucas Silva Braga <lucas.sbraga14@gmail.com>
2022-02-01 13:48:13 -0300

    Security, Spring & Test

--- Spring/blogpessoal/src/main/java/br/org/generation/blogpessoal/model/Usuario.java
+++ Spring/blogpessoal/src/main/java/br/org/generation/blogpessoal/model/Usuario.java
@@ -41,6 +41,19 @@ public class Usuario {
 	@JsonIgnoreProperties("usuario")
 	private List<Postagem> postagem;
 
+	
+	// Método contrutor para fazer testes
+	public Usuario(Long id, String nome, String usuario,String senha,String foto) {
+		this.id = id;
+		this.nome = nome;
+		this.usuario = usuario;
+		this.senha = senha;
+		this.foto = foto;
+	}
+	
+	// Método contrutor vazio para fazer testes
+	public Usuario() { }
+
 	public Long getId() {
 		return id;
 	}
